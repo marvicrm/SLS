08155c5d5c8fd4a673c7863c87172f8436b4c919
Marvic Macalintal <vicmacalintal@yahoo.com>
2017-10-28 16:59:03 +0800

    Create simple_loading_screen.js

---
+++ simple_loading_screen.js
@@ -0,0 +1,34 @@
+/**
+ * Simple Loading Screen 
+ *
+ * @author Marvic R. Macalintal
+ * @version v1.0.0
+ */
+ var sls = {
+	begin : function(text) {
+		this.e = Math.random();
+		let blocker = document.createElement('div');
+		blocker.innerHTML = text;
+		blocker.setAttribute('id',this.e);
+		blocker.setAttribute('style',this.css()['blocker_css']);
+		document.body.setAttribute('style',this.css()['body_css']);
+		document.body.appendChild(blocker);
+	},
+	end : function(text='') {
+		let element = document.getElementById(this.e);
+		element.innerHTML = text;
+		setTimeout(function(){
+				document.body.removeChild(element);
+		},1500);
+	},
+	// blocker css styling
+	css : function() {
+		let style = {
+			'blocker_css'	:	`color:#fff;font-size:25px;background:#000;opacity:0.8;
+								position:absolute;height:100%;width:100%;left:0px;top:0px;
+								text-align:center;padding-top:20%;z-index:999999999999999`,
+			'body_css'		:	`overflow:hidden;`
+		}
+		return style;
+	} 
+ }
